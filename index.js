9b2483aa3cf222f52640033d8b192335e1e35272
connorfrendt <cjacksonfx@yahoo.com>
2019-05-24 16:28:39 -0700

    tip calculator works, working on getting it to show on page

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function submitForm() {
-    console.log('Form submitted!');
+var submitForm = document.getElementById('form');
+
+
+function submitButton() {
+    var foodCost = submitForm.elements['foodMoney'].value;
+    var tipCost = submitForm.elements['tipMoney'].value
+    var total = Number(((tipCost / 100) * foodCost)) + Number(foodCost);
+    console.log(total);
+
+    var total = document.getElementById('total');
+    var cost = document.createElement('div');
+    cost.className = 'cost';
+    total.appendChild(cost);
+
 }
