96a0a134765465ba647d56c8c41f2b01cb3f97e1
connorfrendt <cjacksonfx@yahoo.com>
2019-06-03 16:39:29 -0700

    working on rounding the tip and total

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var submitForm = document.getElementById('form');
 
-
 function submitButton() {
-    var foodCost = submitForm.elements['foodMoney'].value;
-    var tipCost = submitForm.elements['tipMoney'].value
-    var total = Number(((tipCost / 100) * foodCost)) + Number(foodCost);
+    var foodValue = submitForm.elements['foodMoney'].value;
+    var tipValue = submitForm.elements['tipMoney'].value
+    var tip = Number(((tipValue / 100) * foodValue));
+    var total = Number(((tipValue / 100) * foodValue)) + Number(foodValue);
+
+    // toFixed(2)
+
     console.log(total);
 
-    var totalFFF = document.getElementById('total');
-    totalFFF.className = 'cost';
-    totalFFF.textContent = '$' + total;
+    var totalCalc = document.getElementById('total');
+    totalCalc.className = 'cost';
+
+    var div1 = document.createElement('div');
+    var div2 = document.createElement('div');
+
+    totalCalc.appendChild(div1);
+    totalCalc.appendChild(div2);
+
+    div1.className = 'div';
+    div2.className = 'div';
+
+    div1.textContent = 'Tip: $' + tip;
+    div2.textContent = 'Total: $' + total;
+
 }
